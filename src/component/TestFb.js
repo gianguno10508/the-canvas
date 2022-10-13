416915f0cc956ebac02bcf47ff4fb2ca2b2e047a
GiangNV <giangnv@arrowhitech.co>
2022-10-13 15:25:28 +0700

    fgf

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -1,8 +1,8 @@
 import { FacebookProvider, EmbeddedPost, Page,Feed, useFacebook } from 'react-facebook';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
-export default function TestFb() {
+export default function EmbeddedPostExample() {
   return (
-    <MessengerCustomerChat pageId="107400511790455" appId="1505620756529166" />
+    <MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
     // <FacebookProvider appId="1505620756529166">
     //     <button disabled={isLoading} onClick={handleClick}>
     //         Login
